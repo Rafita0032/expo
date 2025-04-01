20e704de0b4f7965d8053dd837b362b0005b4101
Douglas Lowder <douglowder@mac.com>
2025-04-01 13:59:53 -0700

    Scoped notifications modules

--- apps/expo-go/ios/Exponent/Versioned/Core/Modules/ExpoGoExpoNotificationsModules.swift
+++ apps/expo-go/ios/Exponent/Versioned/Core/Modules/ExpoGoExpoNotificationsModules.swift
@@ -14,3 +14,63 @@ public final class ExpoGoNotificationsCategoriesModule: CategoriesModule {
     setScopeKey(scopeKey)
   }
 }
+
+public final class ExpoGoNotificationsEmitterModule: EmitterModule {
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:scopeKey:) instead")
+  }
+
+  required init(appContext: AppContext, scopeKey: String) {
+    super.init(appContext: appContext)
+    setScopeKey(scopeKey)
+  }
+}
+
+public final class ExpoGoNotificationsHandlerModule: HandlerModule {
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:scopeKey:) instead")
+  }
+
+  required init(appContext: AppContext, scopeKey: String) {
+    super.init(appContext: appContext)
+    setScopeKey(scopeKey)
+  }
+}
+
+public final class ExpoGoNotificationsPresentationModule: PresentationModule {
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:scopeKey:) instead")
+  }
+
+  required init(appContext: AppContext, scopeKey: String) {
+    super.init(appContext: appContext)
+    setScopeKey(scopeKey)
+  }
+}
+
+public final class ExpoGoNotificationsSchedulerModule: SchedulerModule {
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:scopeKey:) instead")
+  }
+
+  required init(appContext: AppContext, scopeKey: String) {
+    super.init(appContext: appContext)
+    setScopeKey(scopeKey)
+  }
+}
+
+public final class ExpoGoNotificationsServerRegistrationModule: ServerRegistrationModule {
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:scopeKey:) instead")
+  }
+
+  required init(appContext: AppContext, scopeKey: String) {
+    super.init(appContext: appContext)
+    setScopeKey(scopeKey)
+  }
+}
